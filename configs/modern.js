8edc48bffe0af5de0397804c5b9d01b0e8c2fea3
qimijoy <qimijoy@yandex.ru>
2023-11-07 19:47:43 +0300

    update modern config

--- configs/modern.js
+++ configs/modern.js
@@ -1,11 +1,12 @@
-// Coverage % are taken from  https://browsersl.ist/
-// Date: September 7, 2023
-// Global:    84.9%
-// Russia:    69.5%
+// Coverage % are taken from  https://browsersl.ist/#q=last+10+versions%0Anot+dead%0Anot+op_mini+all
+// Date: November 7, 2023
+// Global:    90.2%
+// Russia:    70.4%
 
 module.exports = [
 	'last 10 versions',
 	'not dead',
+	'not op_mini all',
 ];
 
 
